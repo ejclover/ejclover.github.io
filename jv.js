7412ba1c139002c112fbfc681e1c34e751830ce2
ejclover <smithearl37@gmail.com>
2016-04-21 22:17:20 +0000

    added place

--- jv.js
+++ jv.js
@@ -7,26 +7,28 @@ $(document).ready(function(){
     
     $.get("https://api.foursquare.com/v2/venues/search?client_id=SNQKKXSZ0HF3JE4YU2CMQ43A321ZLE4AXCBFCXYTQ1EJN2PU&client_secret=GVTUZ2P4R4N0DHZ4WSXSOY4KRSDQG4JOTFLB13SPATIYDOII&v=20160213&near=" + $("#ip").val() + "&query=" + $("#q").val() + "&limit=10", function(data){
       for(i=0;i< data.response.venues.length;i++){
-      
-      $("#print").append("<li class =search>"+ data.response.venues[i].name+"</li>");
-      
-     console.log(data.response.venues[i].location.lat);
-     
-      var place = { 
+        var place2 = { 
         lat : data.response.venues[i].location.lat, 
         lng : data.response.venues[i].location.lng
         
       };
       
+      $("#print").append("<li class =search>"+ data.response.venues[i].name+"</li>");
       
       
-     $(".search").click(this,function(){
+      
+      
        
-       var place = { 
-        lat : data.response.venues[this].location.lat, 
-        lng : data.response.venues[this].location.lng
-        
-      };
+      
+     console.log(data.response);
+     
+      
+      
+      
+     $(".search").click(this, function(){
+       
+     
+       
       
        
        var map;
@@ -34,7 +36,7 @@ $(document).ready(function(){
 $(document).ready(function() {
  var mapOptions = {
       center: new google.maps.LatLng(place),
-      zoom: 1
+      zoom: 18
     };
     map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
@@ -58,8 +60,8 @@ $(document).ready(function() {
       });
           
   
-    
       }
+      
   
   });
   
