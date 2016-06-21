2a1a31ad87e447f46a750f14843811190fd64685
ejclover <smithearl37@gmail.com>
2016-06-21 23:06:43 +0000

    started on directions

--- jv.js
+++ jv.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
  
   
   $("#submit").click(function(){  
+      $("#print").empty();
+       
     
     $.get("https://api.foursquare.com/v2/venues/search?client_id=SNQKKXSZ0HF3JE4YU2CMQ43A321ZLE4AXCBFCXYTQ1EJN2PU&client_secret=GVTUZ2P4R4N0DHZ4WSXSOY4KRSDQG4JOTFLB13SPATIYDOII&v=20160213&near=" + $("#ip").val() + "&query=" + $("#q").val() + "&limit=10", function(data){
       for(i=0;i< data.response.venues.length;i++){
@@ -28,13 +30,30 @@ $(document).ready(function(){
       
       (function(place) {
          $(".search"+i).click(this, function(){
+             
+             $("#map-canvas").css("opacity",1);
            
          
            
-          
+
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
            
-           var map;
+  
+   
+           
     // Creates the map
+    function initialize() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var mapOptions = {
+    zoom: 14,
+    center: pos
+  }
+ 
+  directionsDisplay.setMap(map);
+}
         var mapOptions = {
           center: new google.maps.LatLng(place),
           zoom: 18
@@ -42,6 +61,41 @@ $(document).ready(function(){
         map = new google.maps.Map(document.getElementById("map-canvas"),  mapOptions);
         
         console.log("pizza");
+           
+           if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+       var marker1 = new google.maps.Marker({
+            map: map,
+            position: pos ,
+            title: 'Hello World!'
+        });
+    });
+         };  
+      
+ function calcRoute() {
+  var selectedMode = document.getElementById("mode").value;
+  var request = {
+      origin: pos,
+      destination: place,
+      // Note that Javascript allows us to access the constant
+      // using square brackets and a string value as its
+      // "property."
+      travelMode: google.maps.TravelMode[selectedMode]
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+
+        
+     
+         
         
         
         var marker = new google.maps.Marker({
@@ -55,7 +109,7 @@ $(document).ready(function(){
           });
           
       })(place)
-          
+        
   
       }
       
@@ -69,6 +123,7 @@ $(document).ready(function(){
   $("#clear").click(function(){
     
     $("#print").empty();
+     $("#map-canvas").css("opacity",0);
     
   });
   
